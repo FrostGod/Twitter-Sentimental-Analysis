763f66bd7d25cf85bb7e5b5c3d6a5081a4adc21f
manav2606 <manav_11812024@nitkkr.ac.in>
2022-03-30 01:32:50 +0530

    Added frontend proxy

--- web/src/app/common.service.ts
+++ web/src/app/common.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ export class CommonService {
   constructor(private http: HttpClient, public router: Router) {}
 
   _get(apiUrl: any, success: any, failure: any) {
-    this.http.get(`https://www.google.co.in/`).subscribe(
+    this.http.get(`${environment.serverUrl}${apiUrl}`).subscribe(
       (res) => {
         success(res);
       },
